4c8867f6475de562cb5d3cd50dfb6cc29a4962da
mspaintmaestro <mspaintmaestro@gmail.com>
2012-09-09 02:49:23 +0000

    Added serializer. Use &debug=1 to get readable output from the server.

--- src/serverlib/util.py
+++ src/serverlib/util.py
@@ -1,3 +1,58 @@
-def format_output(thing):
-	#TODO: I'll implement this later along with the deserializer in the client
-	return str(thing).replace('\n', '<br />')
+def format_output(thing, debug):
+	if debug:
+		return str(thing).replace('\n', '<br />')
+	else:
+		return serialize_thing(thing)
+
+def serialize_thing(thing):
+	if thing == None:
+		return serialize_none()
+	t = str(type(thing)).split("'")[1]
+	if t == 'int':
+		return serialize_int(thing)
+	if t == 'long':
+		return serialize_long(thing)
+	if t == 'float':
+		return serialize_float(thing)
+	if t == 'str':
+		return serialize_string(thing)
+	if t == 'list' or t == 'tuple':
+		return serialize_list(thing)
+	if t == 'dict':
+		return serialize_dictionary(thing)
+	if t == 'bool':
+		return serialize_bool(thing)
+	return serialize_none()
+
+def serialize_int(thing):
+	return 'i' + str(thing) + '!'
+
+def serialize_long(thing):
+	return 'l' + str(thing) + '!'
+
+def serialize_float(thing):
+	return 'f' + str(thing) + '!'
+
+def serialize_string(thing):
+	return 's' + str(thing).replace('$', '$$').replace('!', '$b') + '!'
+
+def serialize_list(thing):
+	output = ['t']
+	for item in thing:
+		output.append(serialize_thing(item))
+	output.append('!')
+	return ''.join(output)
+
+def serialize_bool(thing):
+	return 'b1!' if thing else 'b0!'
+
+def serialize_dictionary(thing):
+	output = ['d']
+	for key in thing:
+		output.append(serialize_string(str(key)))
+		output.append(serialize_thing(thing[key]))
+	output.append('!')
+	return ''.join(output)
+
+def serialize_none(thing):
+	return 'n!'
