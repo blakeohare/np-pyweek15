934ed6b076727c645b95abda5ae77b031fe86f88
Cosmologicon <Cosmologicon@4db6e3fa-3014-60a1-9dce-bc1767511f2f>
2012-09-09 14:40:42 +0000

    Refactored fullscreen setting Camera methods moved into camera module Terrain generator now has optional tile borders

--- trunk/src/camera.py
+++ trunk/src/camera.py
@@ -1,17 +1,26 @@
 # Singleton module that controls camera view
 
+from src import settings
 
-tilex, tiley, tilez = 16, 8, 2   # pixel sizes for one tile
-
-
+# Offset of the center of the screen, in pixels
 x0 = 0
 y0 = 0
-sx = 400
-sy = 300
 
 # Give screen coordinates given world coordinates
 def screenpos(x, y, z = 0):
-    return int(x * tilex - x0 + sx/2), int(-y * tiley - y0 - z * tilez + sy/2)
+    return int(x * settings.tilex - x0 + settings.sx/2), int(-y * settings.tiley - y0 - z * settings.tilez + settings.sy/2)
 
+# Move the camera in the direction of the given world coordinates
+def track(x, y, z = 0, f = 0.05):
+    global x0, y0
+    dx, dy = screenpos(x, y, z)
+    dx -= settings.sx // 2
+    dy -= settings.sy // 2
+    x0 += dx * f
+    y0 += dy * f
+
+# Immediately point the camera at the given world coordinates
+def lookat(x, y, z = 0):
+    track(x, y, z, 1)
 
 
