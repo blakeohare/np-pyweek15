6eb2fa1325453d48ef96dea5e1c2c5b81a88b8b3
adrian.cline@gmail.com <adrian.cline@gmail.com@4db6e3fa-3014-60a1-9dce-bc1767511f2f>
2012-09-09 18:39:12 +0000

    fixed the crap that was bad

--- trunk/src/serverlib/executor.py
+++ trunk/src/serverlib/executor.py
@@ -10,7 +10,7 @@ def do_things(action, args):
 	if action == 'register':
 		user = args.get('user', '')
 		password = args.get('password', '')
-		if(sql.query('select user_id from user where name=%s',(user,) )
+		if sql.query('select user_id from user where name=%s',(user,) ):
 			output={'success':False}
 		else:
 			user_id=add_user(user, password)
@@ -20,7 +20,7 @@ def do_things(action, args):
 				output = {'success':False}
 		
 	elif action == 'poll':
-		output='success':True,'sectors':build_sector_response(args)}
+		output={'success':True,'sectors':build_sector_response(args)}
 	elif action == 'echo':
 		output = { 'success': True, 'data': args.get('data', None) }
 	elif action == 'build':
@@ -36,7 +36,7 @@ def do_things(action, args):
 					if sector is None:
 						sector = 'NULL'
 					data='No data yet'
-					sql.insert('insert into event (author_token, sector_xy,type,data) values (%s,%s,%s,%s)',(token, sector,type,data)
+					sql.insert('insert into event (author_token, sector_xy,type,data) values (%s,%s,%s,%s)',(token, sector,type,data))
 					success=True
 			output={'success':success,'sectors':build_sector_response(args)}
 			
@@ -72,10 +72,10 @@ def build_poll_update(args, output):
 		#todo remove client_token
 		if(sector is None):
 			add_to_dictlist(responses,'global',{'data':row['data'],'client_token':client_token, 'event_id':event_id})
-		else
+		else:
 			minval=minimums[sector]
 			if row['event_id']>minval:
-				add_to_dictlist(responses,sector,{'data':row['data'],'client_token':client_token, 'event_id':event_id}
+				add_to_dictlist(responses,sector,{'data':row['data'],'client_token':client_token, 'event_id':event_id})
 	output['sectors']=responses
 	
 def add_to_dictlist(dictlist,key,value):
--- trunk/src/serverlib/sql.py
+++ trunk/src/serverlib/sql.py
@@ -1,4 +1,4 @@
-	import MySQLdb
+import MySQLdb
 
 _connection = None
 _credentials = None
