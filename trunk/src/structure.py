a13daae9c3f19475953dd98f0090c0f7b0a8fb06
Cosmologicon <Cosmologicon@4db6e3fa-3014-60a1-9dce-bc1767511f2f>
2012-09-12 05:27:14 +0000

    Fixed terrain height bug Structures appear in the right place 2x2 structures get a platform

--- trunk/src/structure.py
+++ trunk/src/structure.py
@@ -3,35 +3,57 @@ from src import camera, images, terrain, util
 
 class Structure(object):
 	minicolor = 192, 192, 192
+	size = 1
 	# TODO: handle buildings with bigger footprints than 1x1
 	def __init__(self, user_id, x, y, z=None):
 		self.user_id = user_id
 		self.selected = False
 		self.x, self.y = terrain.toCenterRender(x, y)
-		self.z = terrain.iheight(self.x, self.y) if z is None else z
+		if self.size == 2: self.y -= 1
+		if z is None:
+			if self.size == 1:
+				ps = (-1,0),(0,-1),(1,0),(0,1)
+			elif self.size == 2:
+				ps = (-2,0),(-1,-1),(0,-2),(1,-1),(2,0),(1,1),(0,2),(-1,1)
+			self.z = max(terrain.iheight(self.x+ax, self.y+ay) for ax, ay in ps)
+		else:
+			self.z = z
 	
 	def getModelXY(self):
 		return terrain.toModel(self.x, self.y + 1)
 		
-	def renderplatform(self, screen):
+	def renderplatform(self, screen, looker=None):
 		# TODO: this should probably be cached into an image
-		x, y = self.x, self.y
-		z0, z1, z2, z3 = terrain.ihcorners(x, y)
-		zm = max((z0, z1, z2, z3))
-		p0,p1,p2,p3,p4,p5,p6 = [camera.screenpos(a,b,c) for a,b,c in
-				((x-1,y,zm), (x,y-1,zm), (x+1,y,zm), (x,y+1,zm), (x-1,y,z0), (x,y-1,z1), (x+1,y,z2))]
-		# left
-		pygame.draw.polygon(screen, (0,100,100), (p0,p4,p5,p1))
-		# right
-		pygame.draw.polygon(screen, (0,50,50), (p1,p5,p6,p2))
-		# top
-		pygame.draw.polygon(screen, (0,70,70), (p0,p1,p2,p3))
+		x, y, z = self.x, self.y, self.z
+		# TODO: this better
+		if self.size == 1:
+			z0, z1, z2, z3 = terrain.ihcorners(x, y)
+			p0,p1,p2,p3,p4,p5,p6 = [looker.screenpos(a,b,c) for a,b,c in
+					((x-1,y,z), (x,y-1,z), (x+1,y,z), (x,y+1,z), (x-1,y,z0), (x,y-1,z1), (x+1,y,z2))]
+			leftps = p0, p4, p5, p1
+			rightps = p1, p5, p6, p2
+			topps = p0, p1, p2, p3
+		elif self.size == 2:
+			z0, z1, z2, z3, z4 = [terrain.iheight(x+dx,y+dy) for dx,dy in
+				[(-2,0),(-1,-1),(0,-2),(1,-1),(2,0)]]
+			b0,b1,b2,b3,b4,t0,t1,t2,t3 = [looker.screenpos(x+a,y+b,c) for a,b,c in
+				[(-2,0,z0), (-1,-1,z1), (0,-2,z2), (1,-1,z3), (2,0,z4),
+				 (-2,0,z),(0,-2,z),(2,0,z),(0,2,z)]]
+			leftps = t0, b0, b1, b2, t1
+			rightps = t1, b2, b3, b4, t2
+			topps = t0, t1, t2, t3
 
-	def render(self, screen):
-		px, py = camera.screenpos(self.x, self.y, self.z)
-		if not camera.isvisible(px, py, 100):
+
+		pygame.draw.polygon(screen, (0,100,100), leftps)   # left panel
+		pygame.draw.polygon(screen, (0,50,50), rightps)    # right panel
+		pygame.draw.polygon(screen, (0,70,70), topps)      # top panel
+
+	def render(self, screen, looker=None):
+		looker = looker or camera
+		px, py = looker.screenpos(self.x, self.y, self.z)
+		if not looker.isvisible(px, py, 100):
 			return
-		self.renderplatform(screen)
+		self.renderplatform(screen, looker)
 		path = "buildings/selection/%s.png" if self.selected else "buildings/%s.png"
 		img = images.get_image(path % self.btype)
 		ix, iy = img.get_size()
@@ -49,6 +71,7 @@ class Structure(object):
 
 class Farm(Structure):
 	btype = "farm"
+	size = 2
 class Greenhouse(Structure):
 	btype = "greenhouse"
 class HQ(Structure):
@@ -57,12 +80,14 @@ class MedicalTent(Structure):
 	btype = "medicaltent"
 class Quarry(Structure):
 	btype = "quarry"
+	size = 2
 class Radar(Structure):
 	btype = "radar"
 class Turret(Structure):
 	btype = "turret"
 class WaterTreatment(Structure):
 	btype = "watertreatment"
+	size = 2
 
 def create(user_id, type, x, y):
 	output = None
