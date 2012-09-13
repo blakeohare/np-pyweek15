7c001f7d236f57affb1637fecc08a225c50d1288
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@4db6e3fa-3014-60a1-9dce-bc1767511f2f>
2012-09-13 21:07:43 +0000

    Server enforces building in your own sector.

--- trunk/src/serverlib/build.py
+++ trunk/src/serverlib/build.py
@@ -8,28 +8,34 @@ def do_build(user_id, client_token, last_id, sector, loc, type, poll_afterwards=
 	sector = util.tsanitize(sector)
 	loc = util.tsanitize(loc)
 	last_id = util.parseInt(last_id)
-	if client_token:
-		duplicate = sql.query(
-			'SELECT `client_token` FROM `event` WHERE `client_token` = %s', (client_token,))
-		if not duplicate and validate_building(type, sector, loc):
-			data='Build:%s,%s' % (type, loc)
-			event_id = sql.insert(
-				'INSERT INTO `event` (`client_token`, `sector_xy`, `user_id`, `data`) VALUES (%s, %s, %s, %s)', (client_token, sector, user_id, data))
-			quarry_data = ''
-			if type == 'quarry':
-				resources = 'a c s'.split(' ')
-				distribution = [int(random.random() * 101)]
-				rdist = int(random.random() * (100 - distribution[0] + 1))
-				distribution.append(rdist)
-				distribution.append(100 - sum(distribution))
-				random.shuffle(distribution)
-				
-				quarry_data = 'a' + str(distribution[0]) + 'c' + str(distribution[1]) + 's' + str(distribution[2])
-			sql.insert('INSERT INTO `structure` (`type`, `sector_xy`, `loc_xy`, `user_id`, `event_id`, `data`) VALUES (%s, %s, %s, %s, %s, %s)', (type, sector, loc, user_id, event_id, quarry_data))
-		if poll_afterwards:
-			return poll.do_poll(user_id, str(last_id) + '^' + sector)
+	
+	verify_position = sql.query("SELECT `hq_sector` FROM `user` WHERE `user_id` = " + str(user_id) + " LIMIT 1")
+	if verify_position[0]['hq_sector'] == sector:
+		
+		if client_token:
+			duplicate = sql.query(
+				'SELECT `client_token` FROM `event` WHERE `client_token` = %s', (client_token,))
+			if not duplicate and validate_building(type, sector, loc):
+				data='Build:%s,%s' % (type, loc)
+				event_id = sql.insert(
+					'INSERT INTO `event` (`client_token`, `sector_xy`, `user_id`, `data`) VALUES (%s, %s, %s, %s)', (client_token, sector, user_id, data))
+				quarry_data = ''
+				if type == 'quarry':
+					resources = 'a c s'.split(' ')
+					distribution = [int(random.random() * 101)]
+					rdist = int(random.random() * (100 - distribution[0] + 1))
+					distribution.append(rdist)
+					distribution.append(100 - sum(distribution))
+					random.shuffle(distribution)
+					
+					quarry_data = 'a' + str(distribution[0]) + 'c' + str(distribution[1]) + 's' + str(distribution[2])
+				sql.insert('INSERT INTO `structure` (`type`, `sector_xy`, `loc_xy`, `user_id`, `event_id`, `data`) VALUES (%s, %s, %s, %s, %s, %s)', (type, sector, loc, user_id, event_id, quarry_data))
+			if poll_afterwards:
+				return poll.do_poll(user_id, str(last_id) + '^' + sector)
+		else:
+			return { 'success':False, 'message': "missing client token" }
 	else:
-		return { 'success':False, 'message': "missing client token" }
+		return { 'success': False, 'message': "Cannot build this far from your HQ" }
 
 def validate_building(type, sector, coordinate):
 	#for now, having those is enough
