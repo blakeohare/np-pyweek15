175d8d9b87fb3416cb7a65bc720706ff075e42ae
Cosmologicon <Cosmologicon@4db6e3fa-3014-60a1-9dce-bc1767511f2f>
2012-09-14 23:15:14 +0000

    Alien sprites added to game

--- trunk/src/images.py
+++ trunk/src/images.py
@@ -10,3 +10,17 @@ def get_image(path):
 		img = pygame.image.load(rpath)
 		_images[path] = img
 	return img
+
+
+def spritesheet(path, nx, ny):
+	sheet = get_image(path)
+	r = {}
+	w, h = sheet.get_width() // nx, sheet.get_height() // ny
+	for x in range(nx):
+		for y in range(ny):
+			s = pygame.Surface((w, h)).convert_alpha()
+			s.fill((0,0,0,0))
+			s.blit(sheet, (-w*x, -h*y))
+			r[(x,y)] = s
+	return r
+
