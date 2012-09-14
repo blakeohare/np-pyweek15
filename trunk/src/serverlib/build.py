c98df79b17dc5e337d29b2d63f3e181914aaa1b4
mspaintmaestro@gmail.com <mspaintmaestro@gmail.com@4db6e3fa-3014-60a1-9dce-bc1767511f2f>
2012-09-14 18:54:19 +0000

    Server verifies building placement

--- trunk/src/serverlib/build.py
+++ trunk/src/serverlib/build.py
@@ -37,31 +37,36 @@ def do_build(user_id, client_token, last_id, sector, loc, type, poll_afterwards=
 				'SELECT `client_token` FROM `event` WHERE `client_token` = %s', (client_token,))
 			if not duplicate and validate_building(type, sector, loc):
 				
-				if try_deplete_resources(
-					user_id,
-					settings.building_cost[type][0],
-					settings.building_cost[type][1],
-					settings.building_cost[type][2],
-					settings.building_cost[type][3],
-					settings.building_cost[type][4],
-					settings.building_cost[type][5]):
+				error = verify_placement(type, sector, loc)
+				if error == None:
 					
-					data = 'Build:%s,%s' % (type, loc)
-					event_id = sql.insert(
-						'INSERT INTO `event` (`client_token`, `sector_xy`, `user_id`, `data`) VALUES (%s, %s, %s, %s)', (client_token, sector, user_id, data))
-					quarry_data = ''
-					if type == 'quarry':
-						resources = 'a c s'.split(' ')
-						distribution = [int(random.random() * 101)]
-						rdist = int(random.random() * (100 - distribution[0] + 1))
-						distribution.append(rdist)
-						distribution.append(100 - sum(distribution))
-						random.shuffle(distribution)
+					if try_deplete_resources(
+						user_id,
+						settings.building_cost[type][0],
+						settings.building_cost[type][1],
+						settings.building_cost[type][2],
+						settings.building_cost[type][3],
+						settings.building_cost[type][4],
+						settings.building_cost[type][5]):
 						
-						quarry_data = 'a' + str(distribution[0]) + 'c' + str(distribution[1]) + 's' + str(distribution[2])
-					sql.insert('INSERT INTO `structure` (`type`, `sector_xy`, `loc_xy`, `user_id`, `event_id`, `data`) VALUES (%s, %s, %s, %s, %s, %s)', (type, sector, loc, user_id, event_id, quarry_data))
+						data = 'Build:%s,%s' % (type, loc)
+						event_id = sql.insert(
+							'INSERT INTO `event` (`client_token`, `sector_xy`, `user_id`, `data`) VALUES (%s, %s, %s, %s)', (client_token, sector, user_id, data))
+						quarry_data = ''
+						if type == 'quarry':
+							resources = 'a c s'.split(' ')
+							distribution = [int(random.random() * 101)]
+							rdist = int(random.random() * (100 - distribution[0] + 1))
+							distribution.append(rdist)
+							distribution.append(100 - sum(distribution))
+							random.shuffle(distribution)
+							
+							quarry_data = 'a' + str(distribution[0]) + 'c' + str(distribution[1]) + 's' + str(distribution[2])
+						sql.insert('INSERT INTO `structure` (`type`, `sector_xy`, `loc_xy`, `user_id`, `event_id`, `data`) VALUES (%s, %s, %s, %s, %s, %s)', (type, sector, loc, user_id, event_id, quarry_data))
+					else:
+						return { 'success': False, 'message': "Insufficient resources" }
 				else:
-					return { 'success': False, 'message': "Insufficient resources" }
+					return { 'success': False, 'message': error }
 			if poll_afterwards:
 				return poll.do_poll(user_id, str(last_id) + '^' + sector)
 		else:
@@ -72,12 +77,43 @@ def do_build(user_id, client_token, last_id, sector, loc, type, poll_afterwards=
 def validate_building(type, sector, coordinate):
 	#for now, having those is enough
 	if type and sector and coordinate:
-		x,y = map(int, coordinate.split('^'))
-		others = sql.query("SELECT * FROM `structure` WHERE `sector_xy` = %s", (sector,))
-		for other in others:
-			ox,oy = map(int, other['loc_xy'].split('^'))
-			otype = other['type']
-			# TODO: make sure that x,y,type does not conflict geographically with ox,oy,otype
-			# Buildings cannot be placed next to each other. 
-		return True
+		if type in settings.building_cost.keys():
+			x,y = map(int, coordinate.split('^'))
+			others = sql.query("SELECT * FROM `structure` WHERE `sector_xy` = %s", (sector,))
+			for other in others:
+				ox,oy = map(int, other['loc_xy'].split('^'))
+				otype = other['type']
+				# TODO: make sure that x,y,type does not conflict geographically with ox,oy,otype
+				# Buildings cannot be placed next to each other. 
+			return True
 	return False
+
+def verify_placement(type, sector, loc):
+	too_close = "Cannot place building next to another building"
+	edge_of_sector = "Cannot build this far from your HQ"
+	
+	sx, sy = map(int, sector.split('^'))
+	x, y = map(int, loc.split('^'))
+	
+	if x >= 59 or y >= 59: return edge_of_sector
+	
+	size = settings.building_size[type]
+	
+	occupied_tiles = {}
+	
+	existing = sql.query("SELECT `type`, `loc_xy` FROM `structure` WHERE `sector_xy` = %s", (sector,))
+	for existingb in existing:
+		bx, by = map(int, existingb['loc_xy'].split('^'))
+		bs = settings.building_size[existingb['type']]
+		
+		for px in range(bx - 1, bx + bs + 1):
+			for py in range(by - 1, by + bs + 1):
+				occupied_tiles[(px, py)] = True
+	
+	for px in range(x, x + size):
+		for py in range(y, y + size):
+			if occupied_tiles.get((px, py), None) != None:
+				return too_close
+	
+	return None
+	
